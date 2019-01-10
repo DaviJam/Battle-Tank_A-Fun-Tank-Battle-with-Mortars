a05392e97aacd96e31963a7670b14e98ea2eefdb
Purple Dude <daori971@gmail.com>
2019-01-10 11:06:21 +0100

    backup

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+#include "Classes/GameFramework/PlayerController.h"
+
+ATank* ATankAIController::GetControlledTank() const{return Cast<ATank>(GetPawn());}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
+	return (!PlayerPawn)?nullptr:Cast<ATank>(PlayerPawn);
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIcontroller can't find Player tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s AIcontroller found player: %s"),*GetControlledTank()->GetName(), *PlayerTank->GetName())
+	}
+
+}
+
+void ATankAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//AimTowardsCrosshair();
+
+	UE_LOG(LogTemp, Warning, TEXT("AI controller ticking"))
+
+}
+
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+#include "Classes/GameFramework/Controller.h"
+#include "BattleTank.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto Controller = GetControlledTank();
+	if (!Controller)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not possessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possessing: %s"), *Controller->GetName())
+	}
+
+	
+
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+
+
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	//Get world location if linetrace through crosshair
+	//if it hits the landscape
+	//Tell controlled tank to aim at this point
+}
